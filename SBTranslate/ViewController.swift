c6dddf710044b2bb4a000e313e32a0934f6e6fb5
liumengdi <liumengdidd@gmail.com>
2018-12-18 16:20:00 +0800

    init project

---
+++ SBTranslate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SBTranslate
+//
+//  Created by 刘梦迪 on 2018/12/18.
+//  Copyright © 2018 刘梦迪. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
